179c7f9594e6744b05f92df2b7440cea53eaecb3
Kevin Tate <2kmtate@gmail.com>
2020-04-27 19:20:52 -0400

    more shtuff

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
   document.addEventListener('DOMContentLoaded', function() {
     // https://github.com/cferdinandi/smooth-scroll
     var scroll = new SmoothScroll('a.smooth-scroll', {
-      speed: 500
+      speed: 1000
     });
 
     // https://github.com/michalsnik/aos
